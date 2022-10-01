46cc8e6ff57696b13713edfd0622089d643f60b9
mario-welliba <mario.welliba@welliba.com>
2022-10-01 16:06:56 +0200

    styling

--- src/pages/Page2/Page2.tsx
+++ src/pages/Page2/Page2.tsx
@@ -1,12 +1,13 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/system/Box';
 
 import { gql, useQuery } from '@apollo/client';
 
 import Meta from '@/components/Meta';
 import QueryResult from '@/components/QueryResult';
-import { FullSizeCenteredFlexBox } from '@/components/styled';
 import { Todo } from '@/config/types';
 
 const GET_TODO_QUERY = gql`
@@ -25,16 +26,20 @@ function Page2() {
   return (
     <>
       <Meta title="page 2" />
-      <FullSizeCenteredFlexBox>
-        <QueryResult error={error} loading={loading} data={data}>
-          {data?.todos.map((todos: Todo) => (
-            <Card sx={{ minWidth: 345 }} key={todos.id}>
-              <CardHeader title={todos.title}></CardHeader>
-              <CardContent>{todos.text}</CardContent>
-            </Card>
-          ))}
-        </QueryResult>
-      </FullSizeCenteredFlexBox>
+      <Box sx={{ width: '100%' }}>
+        <Grid container rowSpacing={1} columnSpacing={1}>
+          <QueryResult error={error} loading={loading} data={data}>
+            {data?.todos.map((todos: Todo) => (
+              <Grid key={todos.id} item xs={3}>
+                <Card key={todos.id}>
+                  <CardHeader title={todos.title}></CardHeader>
+                  <CardContent>{todos.text}</CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </QueryResult>
+        </Grid>
+      </Box>
     </>
   );
 }
