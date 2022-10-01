b78b86900a4ee033866cd95ae297dbb48d6c8a5a
mario-welliba <mario.welliba@welliba.com>
2022-10-01 12:12:25 +0200

    add login

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,21 +3,33 @@ import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { RecoilRoot } from 'recoil';
 
+import { NhostClient, NhostReactProvider } from '@nhost/react';
+import { NhostApolloProvider } from '@nhost/react-apollo';
+
 import ThemeProvider from '@/theme/Provider';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
+const nhost = new NhostClient({
+  subdomain: 'filjkngpdsoyrxndnepq',
+  region: 'eu-central-1',
+});
+
 function render(App: ComponentType) {
   root.render(
     <StrictMode>
-      <RecoilRoot>
-        <HelmetProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </HelmetProvider>
-      </RecoilRoot>
+      <NhostReactProvider nhost={nhost}>
+        <NhostApolloProvider nhost={nhost}>
+          <RecoilRoot>
+            <HelmetProvider>
+              <ThemeProvider>
+                <App />
+              </ThemeProvider>
+            </HelmetProvider>
+          </RecoilRoot>
+        </NhostApolloProvider>
+      </NhostReactProvider>
     </StrictMode>,
   );
 }
