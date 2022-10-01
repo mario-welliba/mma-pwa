9576b39bf27008b35741cfcf3bb65f573787e28a
mario-welliba <mario.welliba@welliba.com>
2022-10-01 17:41:17 +0200

    Updates

---
+++ assets/index.3f1b3104.js
@@ -0,0 +1,1 @@
+import{M as e}from"./Meta.6f330431.js";import{F as a,T as t}from"./App.52bf2c03.js";import{F as i,L as o,j as r}from"./index.eddb289e.js";import"./index.module.5958740e.js";import"./index.4f3d6e20.js";function d(){return i(o,{children:[r(e,{title:"page 1"}),r(a,{children:r(t,{variant:"h3",children:"Page 1"})})]})}export{d as default};
