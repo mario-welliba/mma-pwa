9f0c556fe263d4b33503a895dc63e1fa323b977a
mario-welliba <mario.welliba@welliba.com>
2022-10-01 12:41:08 +0200

    added Protectetd Route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+import { useAuthenticationStatus } from '@nhost/react';
+
+type ProtectedRouteProps = {
+  children: JSX.Element;
+};
+
+function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { isAuthenticated } = useAuthenticationStatus();
+  const location = useLocation();
+  if (!isAuthenticated) {
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
